0bafa692c713c14f63265fe236617d83a8c292fc
D-Sushma <99126858+D-Sushma@users.noreply.github.com>
2022-07-26 15:18:57 +0530

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@
 //  "/"-->1st page--> ye hame direct show krega brouser me
 // "/content"-->this is route -->browser k is link k sath "/content" add krne pr iska content show hoga 
 // 5. <Link> --> kisi bhi btn pr lga skte hai jisse hm is btn ko click krne pr ek se dusre page me chale jaye-->(go Login.js)
+
 import React from "react";
 import "./style.css";
 import Login from './components/Login'
